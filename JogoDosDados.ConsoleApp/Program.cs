065650767c38854bb17861518a58827f59c0afc8
Natalia Vieira <nataliabortolivieira@gmail.com>
2026-03-19 14:04:23 -0300

    feat: primeira lógica implementada

--- JogoDosDados.ConsoleApp/Program.cs
+++ JogoDosDados.ConsoleApp/Program.cs
@@ -1,9 +1,34 @@
-﻿namespace JogoDosDados.ConsoleApp;
+﻿using System.Security.Cryptography;
+
+namespace JogoDosDados.ConsoleApp;
 
 class Program
 {
-    static void Main(string[] args)
+  static void Main(string[] args)
+  {
+    while (true)
     {
-        Console.WriteLine("Hello, World!");
+
+      Console.Clear();
+      Console.WriteLine("---------------------------------");
+      Console.WriteLine("Jogo dos Dados");
+      Console.WriteLine("---------------------------------");
+
+      Console.Write("Pressione ENTER para lançar um dado...");
+      Console.ReadLine();
+
+      int resultado = RandomNumberGenerator.GetInt32(1, 7);
+
+      Console.WriteLine("---------------------------------");
+      Console.WriteLine($"O número sorteado foi: {resultado}");
+      Console.WriteLine("---------------------------------");
+
+      Console.Write("Deseja continuar? [s/N]: ");
+      string? opcaoContinuar = Console.ReadLine()?.ToUpper();
+
+      if (opcaoContinuar != "S")
+        break;
+
     }
+  }
 }
